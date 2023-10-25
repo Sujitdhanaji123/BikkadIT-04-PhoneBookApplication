254f5ade72fbe9a77b72d56cd59789b053069fcc
Sujitdhanaji123 <dhanajisubhadranandan123@gmail.com>
2023-10-25 19:38:51 +0530

    Whole Project Added

--- src/main/java/com/BikkadIT/PhoneBookApplication/Controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/Controller/ContactController.java
@@ -1,8 +1,104 @@
 package com.BikkadIT.PhoneBookApplication.Controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.BikkadIT.PhoneBookApplication.Entity.Contact;
+import com.BikkadIT.PhoneBookApplication.Service.ContactServiceI;
+
 
 @RestController
 public class ContactController {
+
+	@Autowired
+	private ContactServiceI contactServiceI;
+
+	@PostMapping(value = "/saveContact", consumes = "application/json")
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact) {
+
+		boolean saveContact = contactServiceI.saveContact(contact);
+
+		if (saveContact) {
+			String msg = "Contact Saved Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Saved Successfully", HttpStatus.CREATED);
+
+		}
+	}
+
+	@GetMapping(value = "getAllContact", produces = "application/json")
+	public ResponseEntity<List<Contact>> getAllContact() {
+
+		List<Contact> allContact = contactServiceI.getAllContact();
+		Stream<Contact> stream = allContact.stream();
+		Stream<Contact> filter = stream.filter((contact) -> contact.getActiveSwitch() == 'Y');
+		List<Contact> list = filter.collect(Collectors.toList());
+
+		return new ResponseEntity<List<Contact>>(list, HttpStatus.OK);
+
+	}
+
+	@GetMapping(value = "/getContactById/{contactId}", produces = "application/json")
+	public ResponseEntity<Contact> getContactById(@PathVariable Integer contactId) {
+		Contact contact = contactServiceI.getContactById(contactId);
+		return new ResponseEntity<Contact>(contact, HttpStatus.OK);
+
+	}
+
+	@PutMapping(value = "/updateContact", consumes = "application/json")
+	public ResponseEntity<String> updateContact(@RequestBody Contact contact) {
+
+		boolean saveContact = contactServiceI.updateContact(contact);
+
+		if (saveContact) {
+			String msg = "Contact Updated  Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Updated Successfully", HttpStatus.CREATED);
+
+		}
+
+	}
+
+	@DeleteMapping(value = "/deleteContact/{contactId}")
+	public ResponseEntity<String> deleteContact(@PathVariable Integer contactId) {
+		boolean deleteContact = contactServiceI.deleteContact(contactId);
+
+		if (deleteContact) {
+			String msg = "Contact Deleted  Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Deleted Successfully", HttpStatus.CREATED);
+
+		}
+
+	}
+
+	@DeleteMapping(value = "/deleteContactsoft/{contactId}")
+	public ResponseEntity<String> deleteContactSoft(@PathVariable Integer contactId) {
+		boolean deleteContact = contactServiceI.deleteConatctSoft(contactId);
+
+		if (deleteContact) {
+			String msg = "Contact Deleted  Successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Contact not Deleted Successfully", HttpStatus.CREATED);
+
+		}
+
+	}
 
 }
--- src/main/java/com/BikkadIT/PhoneBookApplication/Service/ContactServiceImpl.java
+++ src/main/java/com/BikkadIT/PhoneBookApplication/Service/ContactServiceImpl.java
@@ -1,8 +1,81 @@
 package com.BikkadIT.PhoneBookApplication.Service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.BikkadIT.PhoneBookApplication.Entity.Contact;
+import com.BikkadIT.PhoneBookApplication.Repository.ContactRepository;
+
+
 @Service
-public class ContactServiceImpl implements ContactServiceI{
+public class ContactServiceImpl implements ContactServiceI {
+
+	@Autowired
+	private ContactRepository contactRepository;
+
+	@Override
+	public boolean saveContact(Contact contact) {
+		Contact contact2 = contactRepository.save(contact);
+
+		if (contact2 != null) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	public List<Contact> getAllContact() {
+
+		List<Contact> list = contactRepository.findAll();
+		return list;
+	}
+
+	@Override
+	public Contact getContactById(Integer id) {
+		Contact id2 = contactRepository.findById(id).get();
+		return id2;
+	}
+
+	@Override
+	public boolean updateContact(Contact contact) {
+		Contact contact2 = contactRepository.save(contact);
+
+		if (contact2 != null) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	public boolean deleteContact(Integer id) {
+
+		boolean existsById = contactRepository.existsById(id);
+		if (existsById) {
+			contactRepository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean deleteConatctSoft(Integer id) {
+		
+		Contact contact = contactRepository.findById(id).get();
+		if(contact !=null) {
+		
+			contact.setActiveSwitch('N');
+			contactRepository.save(contact);
+			return true;
+		}else
+		
+		return false;
+	}
 
 }
